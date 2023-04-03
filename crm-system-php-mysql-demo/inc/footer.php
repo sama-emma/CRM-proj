b86d319f1ac42b4f928e903a489fc1311c092312
sama-emma <113170653+sama-emma@users.noreply.github.com>
2023-04-03 17:33:07 +0100

    Add files via upload

---
+++ crm-system-php-mysql-demo/inc/footer.php
@@ -0,0 +1,9 @@
+<div class="insert-post-ads1" style="margin-top:20px;">
+    <p style="padding: 25px; margin: 10px; width: 100%; background-color: #eee; text-align: center; text-decoration: none;">
+        &copy; copyright <?= date('Y')?> SFM Technology
+    </p>
+</div>
+
+</body>
+</html>
+
---
+++ crm-system-php-mysql-demo/logout.php
@@ -0,0 +1,13 @@
+<?php 
+session_start();
+$_SESSION["userid"] = '';
+
+session_destroy();
+header("Location:index.php");
+?>
+
+
+
+
+
+
